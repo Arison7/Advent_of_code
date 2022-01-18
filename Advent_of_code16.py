c07b8c7ad59cd3e69d44be2ca7e80f3b3597b18c
Arison <arison646@gmail.com>
2022-01-18 17:21:51 +0100

    finish day 16

--- Advent_of_code16.py
+++ Advent_of_code16.py
@@ -1,81 +1,98 @@
-
-
-
+from math import prod
 class parsing:
+    #TODO:delete packages after i am done
+    #TODO: well lucks second subpackage
+    #class variable that folds all the packages for easy version summing
     packages = []
+    functions = [sum,prod,min,max,None,lambda x: int(x[0] > x[1]) ,lambda x: int(x[0] < x[1])  ,lambda x: int(x[0] == x[1])]
+
     def __init__(self,binary_representation:str) -> None:
         self.success = False
+        # 11 is the smallest possible length of package thus if len of binary_representation 
+        # is lower than that we can simple give success a value of false and stop parsing 
         if len(binary_representation) < 11:
             return 
         self.version = int(binary_representation[:3],2)
         self.id_ = int(binary_representation[3:6],2)
         self.binary_representation = binary_representation[6:]
-        self.literial_value = ""
+        self.literial_value = ''
         self.length_id = None
         self.special_number = 0
         self.success = True
+        self.children = []
         parsing.packages.append(self)
-       
+
+    #follows the rules of literal package, determinates package end and returns it
     def initialize_literal(self,i,j):
         group = self.binary_representation[i:j]
         self.literial_value += group[1:]
         if group[0] == '0':
             return j + 6
         return self.initialize_literal(i+5,j+5) 
-        
+    
+    #follows the rules of operator package, determinates package end and returns it
     def initialize_operator(self):
         if self.binary_representation[0] == '0':
             return self.size_mode()
         else: 
             return self.amount_mode()
-            
+
+    #given the special number that is a legth of all of it's subpakages it's initializing all of them and returns end of whole thing  
     def size_mode(self):
         self.special_number = int(self.binary_representation[1:16],2)
         new_sub_packet = parsing(self.binary_representation[16:16+self.special_number])
         break_point = new_sub_packet.calculate()
-        while 16+self.special_number -(16+break_point) > 6:
+        self.children.append(new_sub_packet)
+        while 16+self.special_number -(16+break_point) >= 11:
             new_sub_packet = parsing(self.binary_representation[16+break_point:16+self.special_number])
             if not new_sub_packet.success:
                 break
+            self.children.append(new_sub_packet)
             break_point += new_sub_packet.calculate()
+        #22 is 3 for versions + 3 for id + 1 for length id + 15 for the special number thus the size of it 
         return self.special_number + 22
-    
+
+    #given the special number that is amount of it's subpakages it's initializing all of them and returns end of whole thing
     def amount_mode(self):
         self.special_number = int(self.binary_representation[1:12],2)
         new_sub_packet = parsing(self.binary_representation[12:])
         break_point = new_sub_packet.calculate()
-
+        self.children.append(new_sub_packet)
         self.special_number -= 1
         while self.special_number != 0:
             new_sub_packet = parsing(self.binary_representation[12+break_point:])
             if not new_sub_packet.success:
                 break
+            self.children.append(new_sub_packet)
             break_point += new_sub_packet.calculate()
             self.special_number -= 1
+        #break_point is the end of subpackages
+        #18 is 3 for versions + 3 for id + 1 for length id + 11 for the special number thus amount 
         return break_point + 18
     
-
     def calculate(self):
         if self.id_ == 4:
             return self.initialize_literal(0,5)
         else:
             return self.initialize_operator()
+    def __call__(self):
+        if self.id_ == 4:
+            return int(self.literial_value,2)
+        to_return : list = []
+        for child in self.children:
+            to_return.append(child())
+        return self.functions[self.id_](to_return)
     
         
-        
-        
+    
+                   
 def get_data(path):
     data = ''
     with open(path, 'r') as f:
         for line in f.readlines():
             data += line.replace('\n', '')
     return data
-            
-           
-
-        
-
-
+               
 def casting_binary(hex_:str) -> str:
     binary = ""
     for hex_number in hex_:
@@ -85,19 +102,15 @@ def casting_binary(hex_:str) -> str:
     return binary
 
     
-
-
 def main():
-    data : str = get_data(r'')
+    data : str = get_data(r'D:\Coding\Python\handy\cases.txt')
     first = parsing(casting_binary(data))
     first.calculate()
-    print(sum([p.version for p in first.packages]))
+    print(first())
+    
+   
     
   
     
-    
-
-
-
 if __name__ == "__main__":
     main()
